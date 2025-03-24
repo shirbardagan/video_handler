1b89f992677a463a7713d19e092e55893bd5f0ab
shirbardagan <shirberdugo1@gmail.com>
2025-03-24 09:52:48 +0200

    change route

--- app_instance.py
+++ app_instance.py
@@ -4,9 +4,10 @@ from fastapi import FastAPI
 
 app = FastAPI(
     version="1.0.0",
-    docs_url="/api-docs"
+    docs_url="/api-docs",
+    swagger_ui_parameters={"useLocalFiles": True}
 )
 
 
 def start_server():
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
