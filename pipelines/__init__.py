473b3c0fdd2c52adc8d2c37b75e74f7cf0fc369a
shirbardagan <shirberdugo1@gmail.com>
2025-04-08 15:16:58 +0300

    cleaner imports in stream pipeline factory

--- pipelines/__init__.py
+++ pipelines/__init__.py
@@ -1,5 +1,9 @@
 from .base_pipeline import BaseStreamPipeline
 from .mp2t_h264_pipeline import MP2TH264StreamPipeline
+from .mp2t_pipeline import MP2TStreamPipeline
+from .mpeg4i_pipeline import MPEG4IStreamPipeline
+from .test_pipeline import TESTStreamPipeline
+from .v4l2_pipeline import V4L2StreamPipeline
 from .webrtc_pipeline import WebRTCPipeline
 from .rtsp_pipeline import RTSPStreamPipeline
 from .rtp_pipeline import RTPStreamPipeline
@@ -12,5 +16,9 @@ __all__ = [
     RTPStreamPipeline,
     MP2TH265StreamPipeline,
     MP2TH264StreamPipeline,
-    BaseStreamPipeline
+    BaseStreamPipeline,
+    V4L2StreamPipeline,
+    MP2TStreamPipeline,
+    MPEG4IStreamPipeline,
+    TESTStreamPipeline,
 ]
