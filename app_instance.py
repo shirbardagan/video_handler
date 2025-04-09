8c31f923343e52dd09bd4e9660623edfbfb21813
shirbardagan <shirberdugo1@gmail.com>
2025-04-09 17:41:26 +0300

    port dynamically from env var

--- app_instance.py
+++ app_instance.py
@@ -1,13 +1,14 @@
 import uvicorn
 
 from fastapi import FastAPI
+from config_models.system_config import SystemSettingsConfig
 
 app = FastAPI(
     version="1.0.0",
     docs_url="/api-docs",
     swagger_ui_parameters={"useLocalStorage": True}
 )
-
+system_conf = SystemSettingsConfig()
 
 def start_server():
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=system_conf.port)
