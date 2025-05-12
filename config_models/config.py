b1b8618a3aa789beb683384a509b8db089948cf8
shirbardagan <shirberdugo1@gmail.com>
2025-05-12 15:51:39 +0300

    mpeg4i pipeline

--- config_models/config.py
+++ config_models/config.py
@@ -1,5 +1,6 @@
 CAPS_H264="video/x-h264, stream-format=(string)byte-stream"
 CAPS_RTP="application/x-rtp,media=video,clock-rate=90000,encoding-name=H264,payload=96"
+CAPS_MPEG4I = "video/mpeg, width=640, height=480, framerate=60/1, mpegversion=(int)4, systemstream=(boolean)false"
 
 SYSTEM_DEFAULT_VALUE=-1
 SYSTEM_DEFAULT_HOST="127.0.0.1"
